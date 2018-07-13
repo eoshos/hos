3e8862276f30c19c7892a7fb5069c27ee7d43a8c
eoshos <service@eoshos.io>
2018-07-13 11:23:41 +0800

    v0.0.1

--- 02.JavaSource/hos/hos-simple-console/src/main/java/io/eoshos/console/simple/service/IHosUserInviteService.java
+++
@@ -1,8 +0,0 @@
-package io.eoshos.console.simple.service;
-
-import io.eoshos.console.simple.bean.bo.HosUserInviteBo;
-import io.eoshos.console.simple.bean.dto.HosUserInviteDto;
-
-public interface IHosUserInviteService extends IBaseService<HosUserInviteBo, HosUserInviteDto>{
-	
-}
--- 02.JavaSource/hos/hos-simple-console/src/main/java/io/eoshos/console/simple/service/IHosUserTaskAttachmentService.java
+++
@@ -1,9 +0,0 @@
-package io.eoshos.console.simple.service;
-
-import io.eoshos.console.simple.bean.bo.HosUserTaskAttachmentBo;
-import io.eoshos.console.simple.bean.dto.HosUserTaskAttachmentDto;
-
-public interface IHosUserTaskAttachmentService extends IBaseService<HosUserTaskAttachmentBo, HosUserTaskAttachmentDto>{
-	
-	
-}
